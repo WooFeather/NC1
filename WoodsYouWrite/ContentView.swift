13eabae582df88462bbc3be1837e3c599296243b
woboneyo <bluewing1004@gmail.com>
2024-04-11 22:21:19 +0900

    Initial Commit

---
+++ WoodsYouWrite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WoodsYouWrite
+//
+//  Created by 조우현 on 4/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WoodsYouWrite/WoodsYouWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodsYouWriteApp.swift
+//  WoodsYouWrite
+//
+//  Created by 조우현 on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WoodsYouWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
